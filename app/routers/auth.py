7cc3cdb96211e9f9e66b8ad1bab8e90d94a17170
Nadim Naisam <96692352+NadimNaisam@users.noreply.github.com>
2025-01-22 01:52:51 +0530

    changed status codes

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,14 +8,14 @@ import utils
 router = APIRouter(tags=['Authentication'])
 
 #since we use OAuth2PasswordRequestForm here below, if we test in postman we have to send the data not as raw, but as form-data
-@router.post('/login')
+@router.post('/login',response_model=schemas.Token)
 def login(user_credentials:OAuth2PasswordRequestForm = Depends() ,db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()  #username is essentially the email, its just called username cause we used OAuth2PasswordRequestForm
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Invalid Credentials")
     
     if not utils.verify(user_credentials.password,user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Invalid Credentials")
     
     #create token jwt and return
     access_token = oauth2.create_access_token(data={"user_id":user.id})
