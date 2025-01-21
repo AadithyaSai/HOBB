b73ae6d49c0d4fe6ea08f26dce3d1d31232e3e4a
Nadim Naisam <96692352+NadimNaisam@users.noreply.github.com>
2025-01-22 01:54:39 +0530

    added get_current_user()

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,7 +1,11 @@
 from jose import JWTError,jwt
-from datetime import datetime,timedelta
-from . import schemas
+from datetime import datetime,timedelta,timezone
+from . import schemas,database
+from fastapi import Depends,status,HTTPException
+from fastapi.security import OAuth2PasswordBearer
 #secret key, algorithm to use(sha256), expiration time of token(user cant be logged in forever)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')  #tokenurl is the login endpoint..here it is /login
 
 SECRET_KEY = "09d25e094fdikshf556c818166b7a9563b93faidjslf0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -10,16 +14,31 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data:dict):
     to_encode = data.copy()
     
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc).replace(tzinfo=None) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp" : expire})
 
     encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM) #creates token
     return encoded_jwt
 
 def verify_access_token(token:str,credentials_exception):
-    payload = jwt.decode(token,SECRET_KEY,ALGORITHM)
-    id:str = payload.get("users_id")
+    try:
+        payload = jwt.decode(token,SECRET_KEY,[ALGORITHM])
+        id:str = payload.get("user_id")
 
-    if id is None:
+        if id is None:
+           raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+
+    except JWTError:
         raise credentials_exception
-    token_data = schemas.TokenData(id=id)
+    
+    return token_data
+    
+#(below fn)pass as dependency to path opn, thus taking token from reqst automatically,extract id for us, verify token validity 
+def get_current_user(token:str=Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Could not validate the credentials",headers={"WWW-Authenticate":"Bearer"})
+
+    return verify_access_token(token,credentials_exception)
+
+#anytime we have an endpoint that needs to be protected (user needs to be authorized to access it), we add the above fn as a dependency, so token needs to be provided to access that endpoint
+#68 01:35
